ba16bba5ffea54d9222fe6262cd2bb1746c69350
権藤　俊 <gondo.syun0711@gmail.com>
2021-01-10 15:31:18 +0900

    Initial Commit

---
+++ BarcodeApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarcodeApps
+//
+//  Created by しゅん on 2021/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
